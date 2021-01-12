f5bed7b3efdd20cb03ca51357c3c96fec0f0dea6
Baduit <simon.bertho@epitech.eu>
2021-01-12 02:10:30 +0100

    The visitor is now exhaustive

--- lib/PainPerdu/vm/VirtualMachine.cpp
+++ lib/PainPerdu/vm/VirtualMachine.cpp
@@ -19,10 +19,13 @@ void VirtualMachine::compile(Definitions definitions)
 	_definitions += optimize(std::move(definitions));
 }
 
+template<class> inline constexpr bool always_false_v = false;
+
 void VirtualMachine::run()
 {
+	bool run = true;
 	// I use integers instead of iterators to avoid iterator invalidation
-	while (_step < _definitions.recipe.size())
+	while (run && _step < _definitions.recipe.size())
 	{
 		std::visit(
 			[&](const auto& current_instruction)
@@ -31,25 +34,78 @@ void VirtualMachine::run()
 				if constexpr (std::same_as<instructions::MoveRight, T>)
 				{
 					_memory.advance_cursor(current_instruction.value);
+					_step += 1;
 				}
 				else if constexpr (std::same_as<instructions::MoveLeft, T>)
 				{
 					_memory.move_back_cursor(current_instruction.value);
+					_step += 1;
 				}
 				else if constexpr (std::same_as<instructions::Increment, T>)
 				{
 					_memory.incr_current_case(current_instruction.value);
+					_step += 1;
 				}
 				else if constexpr (std::same_as<instructions::Decrement, T>)
 				{
 					_memory.decr_current_case(current_instruction.value);
+					_step += 1;
+				}
+				else if constexpr (std::same_as<instructions::DefineReference, T>)
+				{
+					logger[LogCategory::VM].error("Not implemented yet.");
+					run = false;
+				}
+				else if constexpr (std::same_as<instructions::UndefineReference, T>)
+				{
+					logger[LogCategory::VM].error("Not implemented yet.");
+					run = false;
+				}
+				else if constexpr (std::same_as<instructions::MoveToReference, T>)
+				{
+					logger[LogCategory::VM].error("Not implemented yet.");
+					run = false;
+				}
+				else if constexpr (std::same_as<instructions::GoToLabel, T>)
+				{
+					logger[LogCategory::VM].error("Not implemented yet.");
+					run = false;
+				}
+				else if constexpr (std::same_as<instructions::IfCurrentValueEquals0, T>)
+				{
+					logger[LogCategory::VM].error("Not implemented yet.");
+					run = false;
+				}
+				else if constexpr (std::same_as<instructions::IfCurrentValueEqualsN, T>)
+				{
+					logger[LogCategory::VM].error("Not implemented yet.");
+					run = false;
+				}
+				else if constexpr (std::same_as<instructions::IfCursorIsAtReference, T>)
+				{
+					logger[LogCategory::VM].error("Not implemented yet.");
+					run = false;
+				}
+				else if constexpr (std::same_as<instructions::IfReferenceExists, T>)
+				{
+					logger[LogCategory::VM].error("Not implemented yet.");
+					run = false;
+				}
+				else if constexpr (std::same_as<instructions::GetChar, T>)
+				{
+					logger[LogCategory::VM].error("Not implemented yet.");
+					run = false;
+				}
+				else if constexpr (std::same_as<instructions::PutChar, T>)
+				{
+					logger[LogCategory::VM].error("Not implemented yet.");
+					run = false;
 				}
 				else
 				{
-					logger[LogCategory::VM].error("Not implemented yet.");
+					static_assert(always_false_v<T>, "non-exhaustive visitor!");
 				}
 			}, _definitions.recipe[_step]);
-		++_step;
 	}
 }
 
