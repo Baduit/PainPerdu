6b76541ffd1b1b15a71d930c1549cea3f4c57eaf
Lena Bertho <lena.bertho@protonmail.com>
2022-10-14 21:26:11 +0200

    Support tests on emscripten

--- cmake/addTest.cmake
+++ cmake/addTest.cmake
@@ -1,3 +1,11 @@
+if (EMSCRIPTEN)
+	set(EMSCRIPTEN_COMPILE_FLAG "")
+	set(FORCE_EXCEPTION_FLAG "-fwasm-exceptions")
+else()
+	set(EMSCRIPTEN_COMPILE_FLAG "")
+	set(FORCE_EXCEPTION_FLAG "")
+endif()
+
 function(addTest test_name test_files)
 	set(test_target ${test_name}_test)
 
@@ -6,8 +14,9 @@ function(addTest test_name test_files)
 	target_compile_options(${test_target}
 						PRIVATE
 						$<$<CXX_COMPILER_ID:MSVC>:/W3 /permissive- /TP>
-						$<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>:-Wextra -Wall -Wsign-conversion -Wfloat-equal -pedantic -Wredundant-decls -g>)
+						$<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>:-Wextra -Wall -Wsign-conversion -Wfloat-equal -pedantic -Wredundant-decls -g ${FORCE_EXCEPTION_FLAG} ${EMSCRIPTEN_COMPILE_FLAG}>)
 	target_link_libraries(${test_target} PRIVATE PainPerdu doctest::doctest)                       
+	target_link_options(${test_target} PRIVATE ${FORCE_EXCEPTION_FLAG})
 	target_include_directories(${test_target} PRIVATE ${CMAKE_CURRENT_LIST_DIR})
 	add_test(${test_name} ${test_target})
 endfunction()
