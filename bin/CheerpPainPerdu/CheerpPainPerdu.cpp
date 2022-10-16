fcd075960d0b711d64a885c49cb5d0827ca334ae
Lena Bertho <lena.bertho@protonmail.com>
2022-10-16 19:07:08 +0200

    It almost works with cheerp

---
+++ bin/CheerpPainPerdu/CheerpPainPerdu.cpp
@@ -0,0 +1,12 @@
+#include <cheerp/clientlib.h>
+#include <cheerp/client.h>
+
+[[cheerp::jsexport]] int totol(int n)
+{
+    return n * 2;
+}
+
+void webMain()
+{
+
+}
--- cmake/addTest.cmake
+++ cmake/addTest.cmake
@@ -1,8 +1,8 @@
 if (EMSCRIPTEN)
-	set(EMSCRIPTEN_COMPILE_FLAG "")
 	set(FORCE_EXCEPTION_FLAG "-fwasm-exceptions")
+elseif(CHEERP)
+	set(FORCE_EXCEPTION_FLAG "-fexceptions")
 else()
-	set(EMSCRIPTEN_COMPILE_FLAG "")
 	set(FORCE_EXCEPTION_FLAG "")
 endif()
 
@@ -10,11 +10,10 @@ function(addTest test_name test_files)
 	set(test_target ${test_name}_test)
 
 	add_executable(${test_target} ${test_files})
-	target_compile_features(${test_target} PRIVATE cxx_std_20)
 	target_compile_options(${test_target}
 						PRIVATE
 						$<$<CXX_COMPILER_ID:MSVC>:/W3 /permissive- /TP>
-						$<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>:-Wextra -Wall -Wsign-conversion -Wfloat-equal -pedantic -Wredundant-decls -g ${FORCE_EXCEPTION_FLAG} ${EMSCRIPTEN_COMPILE_FLAG}>)
+						$<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>:-Wextra -Wall -Wsign-conversion -Wfloat-equal -pedantic -Wredundant-decls -g ${FORCE_EXCEPTION_FLAG}>)
 	target_link_libraries(${test_target} PRIVATE PainPerdu doctest::doctest)                       
 	target_link_options(${test_target} PRIVATE ${FORCE_EXCEPTION_FLAG})
 	target_include_directories(${test_target} PRIVATE ${CMAKE_CURRENT_LIST_DIR})
